f0cda883f67ef58de965c197e0d2549ce64744bc
fabulousduck <ryanvlaming@icloud.com>
2017-12-30 15:41:22 +0100

    disallow unnamed functions on namspaces

--- src/kube/namespace.js
+++ src/kube/namespace.js
@@ -8,11 +8,13 @@ module.exports = function namespace(name) {
   this.id = crypto.randomBytes(20).toString('hex');
 
   this.def = function def(fn) {
-    //TODO
+    if(!fn.name || fn.name === '') {
+      throw new Error('Function name not defined');
+    }
+
     if(Object.keys(this).includes(fn.name)) {
       throw new Error('Cannot redefine ' + fn.name);
     }
-    //make check that it cant overwrite default functions
     this[fn.name] = fn;
   };
 };
